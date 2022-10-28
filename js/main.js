c5b295dd1511498c2e83a7cc9ac1f4926326426b
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-10-28 11:32:26 +0000

    Add error handler for when dataset does not exist

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+let dotPlotElementName = "dotplot";
+
 d3.csv("coefficients.csv").then( (data) => {
-    let dotPlot = new DotPlot("dotplot", data);
+    let dotPlot = new DotPlot(dotPlotElementName, data);
     dotPlot.initPlot();
     dotPlot.drawPlot();
+}).catch((error) => {
+    let errorMessage = "An error occurred while loading the dataset. ";
+    errorMessage += "Do you have <code>coefficients.csv</code> in the Rekker directory?";
+    errorMessage += `<br><br>${error}`;
+
+    d3.select(`#${dotPlotElementName}`).html(errorMessage);
 });
