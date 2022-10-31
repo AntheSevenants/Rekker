cafdd7ef298cad2f606adfdb3a544d575ae6c8fd
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-10-31 12:58:17 +0000

    Add ColorCodings class

---
+++ js/ColorCodings.js
@@ -0,0 +1,8 @@
+class ColorCodings {
+	static get PositiveNegative () {
+		return "radio_variable_encoding_positive_negative"
+	}
+	static get GroupCoding () {
+		return "radio_variable_encoding_group_coding"
+	}
+}
