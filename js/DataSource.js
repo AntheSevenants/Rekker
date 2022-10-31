258ce911f79cf18f4cfc1a98597feee8d19f8cb8
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-10-31 09:20:29 +0000

    Implement DataSource class to generalise dataset loading

---
+++ js/DataSource.js
@@ -0,0 +1,34 @@
+class DataSource {
+    constructor() {
+        // We define which datasets *can* be loaded
+        this.datasets = { "coefficients": d3.csv("coefficients.csv"),
+                          "coding": d3.csv("coding.csv") };
+    }
+
+    async load() {
+        // We collect all promises to try to resolve them
+        let promises = Object.values(this.datasets);
+        // We also collect all keys, so we know to what dataset each promise belongs
+        let keys = Object.keys(this.datasets);
+
+        // We give all promises and try whether they resolve
+        await Promise.allSettled(promises).then(results => {
+            let i = 0;
+            // Go over each dataset we defined earlier
+            for (let dataset in this.datasets) {
+                // If the promise was succesful, we retrieve the value of that promise
+                // Else, we set the dataset to null
+                switch (results["status"]) {
+                    case "fulfilled":
+                        this.datasets[keys[i]] = results[i]["value"];
+                        break;
+                    case "rejected":
+                        this.datasets[keys[i]] = null;
+                        break;
+                }
+            }
+        });
+
+        return true;
+    }
+}
